cfa49704876585d17fa8b471baf75e34e46afb27
Nekmayne <bgm035@myy.haaga-helia.fi>
2023-01-23 21:25:35 +0200

    backend changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const Koa = require("koa");
 const router = require("koa-router")();
 const cors = require("kcors");
-const axios = require("axios");
+const fetch = require("node-fetch");
 require("dotenv").config();
 
 const appId = process.env.APPID;
@@ -14,10 +14,31 @@ app.use(cors());
 
 router.get("/weather/:city", async (ctx) => {
   const city = ctx.params.city;
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appId}&units=metric`;
-  const response = await axios.get(url);
-  ctx.body = response.data;
-  ctx.type = "application/json; charset=utf-8";
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appId}&units=metric`
+    );
+    const data = await response.json();
+    ctx.body = data;
+    ctx.type = "application/json; charset=utf-8";
+  } catch (err) {
+    ctx.body = err.message;
+    ctx.status = 500;
+  }
+});
+
+router.get("/location", async (ctx) => {
+  const { lat, lon } = ctx.query;
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appId}&units=metric`
+    );
+    const data = await response.json();
+    ctx.body = data;
+  } catch (err) {
+    ctx.body = err.message;
+    ctx.status = 500;
+  }
 });
 
 app.use(router.routes());
